071285f2e39365300298b4fae12f5bb645e8973c
Jonathan Erdmann <76535224+jonathan-erdmann@users.noreply.github.com>
2020-12-28 17:19:10 -0600

    Initialize repository
    Code is not robust!

---
+++ R/geometricMean.r
@@ -0,0 +1,63 @@
+#library(matrixStats)
+
+binaryOutcomes <- function(n) {
+
+    if (n > 1) {
+
+        nZero    <- 2^n - 1
+        binary32 <- matrix(as.integer(intToBits(0:nZero)), nrow = 32)
+
+        return(t(binary32[1:n,]))
+
+    } else {
+
+        return(matrix(c(0,1), ncol = 1))
+
+    }
+
+}
+
+columnwiseExponentiation <- function(iBase, iExponent) {
+
+    nBase <- length(iBase)
+    ce <- iExponent
+
+    for (column in 1:nBase) {
+
+        base <- iBase[column]
+        exponent <- iExponent[,column]
+
+        ce[,column] <- base ^ exponent
+
+    }
+
+    return(ce)
+
+}
+
+geometricMean <- function(iBets, iProb, iPays) {
+
+    nGames <- length(iBets)
+    outcomes <- binaryOutcomes(nGames)
+
+    likelihood <- columnwiseExponentiation(iProb, outcomes)
+    likelihood <- likelihood * columnwiseExponentiation(1 - iProb, 1 - outcomes)
+    likelihood <- rowProds(likelihood)
+
+    payouts <- (outcomes %*% (iPays * iBets)) - (1 - outcomes) %*% iBets
+
+    if (min(payouts) > -1) {
+
+        logPayouts <- log(1 + payouts)
+        gm <- sum(likelihood %*% logPayouts)
+
+    } else {
+
+        #gm <- -Inf
+        gm <- -1E12
+
+    }
+
+    return(gm)
+
+}
---
+++ R/optimizeBets.R
@@ -0,0 +1,40 @@
+optimizeBets <- function (iProbabilities, iPayouts) {
+
+    #Eliminate non-viable bets
+    viableBets <- iProbabilities * (iPayouts + 1) - 1 > 0
+    viableProb <- iProbabilities[viableBets]
+    viablePays <- iPayouts[viableBets]
+
+    nBets     <- length(viableProb)
+    initBets  <- runif(nBets, min = 1, max = 10) / 1000
+    optimFunc <- function(x) -geometricMean(x, viableProb, viablePays)
+
+    res <- optim(
+             initBets
+            ,optimFunc
+            ,lower = rep(1E-8, nBets)
+            ,upper = rep(1 - 1E-8, nBets)
+            ,method = "L-BFGS-B"
+        )
+
+    res$viableBets        <- viableBets
+    res$individualKelly   <- kellyBet(iProbabilities, iPayouts)
+    res$individualExp     <- iProbabilities * (iPayouts + 1)
+    res$probability       <- iProbabilities
+    res$payouts           <- iPayouts
+    res$viableProbability <- viableProb
+    res$viablePays        <- viablePays
+    res$value             <- -res$value
+
+    return(res)
+
+}
+
+kellyBet <-  function (iProbabilities, iPayouts) {
+
+    q <- 1 - iProbabilities
+    f <- (iPayouts * iProbabilities - q) / iPayouts
+
+    return(f)
+
+}
