810b46612a2dee0fa1066fbace5cee511e4e0c57
Jonathan Erdmann <76535224+jonathan-erdmann@users.noreply.github.com>
2021-01-06 12:54:47 -0600

    moved to probabilityToMoneyLine.R

--- R/requiredMoneyLine.R
+++
@@ -1,8 +0,0 @@
-#' @export
-requiredMoneyLine <- function(iProbability) {
-
-    moneyLine <- ifelse(iProbability > 0.5, -100 / (1 / iProbability - 1),   100 * (1 / iProbability - 1))
-
-    return(moneyLine)
-
-}
