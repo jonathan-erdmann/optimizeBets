93b9b161d9f4947c1a4fe436c1e9b41c2f3f20e3
jonathan-erdmann <jonathan.erdmann@protonmail.com>
2021-01-15 14:05:44 -0600

    Removing number of games limitation

--- R/makeBetCard.R
+++ R/makeBetCard.R
@@ -1,23 +1,18 @@
 #' @export
-makeBetCard <- function(iFile, iLimitGamesToBet) {
+makeBetCard <- function(iFile) {
 
     betIn <- readBetProbabilityOdds(iFile)
     betIn <- betIn[betIn$kellyBet > 0, ]
     nViable <- nrow(betIn)
 
     betIn <- betIn[order(-betIn$kellyBet), ]
-    if (nViable > iLimitGamesToBet) {
-
-        betIn <- betIn[1:iLimitGamesToBet, ]
-
-    }
-
     betOut <- optimizeBets(betIn)
 
     betInViable <- betIn[betOut$bet > 0, ]
     betOutViable <- betOut[betOut$bet > 0, ]
 
     betCard <- betInViable %>% left_join(betOutViable, by = "name")
+    betCard <- betCard[order(-bet), ]
 
     return(betCard)
 
