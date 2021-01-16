35ee99bb85e3fc7689d76981640f18ec134ccb0c
jonathan-erdmann <jonathan.erdmann@protonmail.com>
2021-01-15 18:40:04 -0600

    Simplifying makeBetCard function

--- R/makeBetCard.R
+++ R/makeBetCard.R
@@ -2,16 +2,11 @@
 makeBetCard <- function(iFile, iExact = FALSE, iFactorBet = 4, iMinBetSize = 0.0005) {
 
     betIn <- readBetProbabilityOdds(iFile)
-    betIn <- betIn[betIn$kellyBet > 0, ]
-    nViable <- nrow(betIn)
+    betIn <- betIn %>% filter(kellyBet > 0)
 
-    betIn <- betIn[order(-betIn$kellyBet), ]
     betOut <- optimizeBets(betIn, iExact)
 
-    betInViable <- betIn[betOut$bet > 0, ]
-    betOutViable <- betOut[betOut$bet > 0, ]
-
-    betCard <- betInViable %>%
+    betCard <- betOut %>%
         mutate(bet = bet / iFactorBet)
         left_join(betOutViable, by = "name") %>%
         arrange(-bet) %>%
