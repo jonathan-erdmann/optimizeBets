3e64d5bc1bec1006dbc66a18fbb3fe7265484fc1
Jonathan Erdmann <76535224+jonathan-erdmann@users.noreply.github.com>
2022-08-30 17:54:49 -0500

    Adding verbosity option

--- R/getBettingResult.R
+++ R/getBettingResult.R
@@ -1,5 +1,5 @@
 #' @export
-getBettingResult <- function(iBetCard, iStartingCapital) {
+getBettingResult <- function(iBetCard, iStartingCapital, iVerbose = FALSE) {
 
     wagered <- iBetCard$bet * iStartingCapital
     odds <- moneyLineToOdds(iBetCard$moneyLine)
@@ -15,14 +15,16 @@ getBettingResult <- function(iBetCard, iStartingCapital) {
     returnOnWager <- 100 * (totalReturned / totalWagered - 1)
     returnOnCapital <- 100 * (endingCapital / iStartingCapital - 1)
 
-    print(noquote(sprintf(" Starting Capital : $%.2f", iStartingCapital)))
-    print(noquote(sprintf("    Total Wagered : $%.2f", totalWagered)))
-    print(noquote(sprintf("Remaining Capital : $%.2f", remainingCapital)))
-    print(noquote(sprintf("   Money Returned : $%.2f", totalReturned)))
-    print(noquote(sprintf("       Proft/Loss : $%.2f", netGain)))
-    print(noquote(sprintf("   Ending Capital : $%.2f", endingCapital)))
-    print(noquote(sprintf("  Return on Wager : %.2f", returnOnWager)))
-    print(noquote(sprintf("Return on Capital : %.2f", returnOnCapital)))
+    if (iVerbose) {
+	    print(noquote(sprintf(" Starting Capital : $%.2f", iStartingCapital)))
+	    print(noquote(sprintf("    Total Wagered : $%.2f", totalWagered)))
+	    print(noquote(sprintf("Remaining Capital : $%.2f", remainingCapital)))
+	    print(noquote(sprintf("   Money Returned : $%.2f", totalReturned)))
+	    print(noquote(sprintf("       Proft/Loss : $%.2f", netGain)))
+	    print(noquote(sprintf("   Ending Capital : $%.2f", endingCapital)))
+	    print(noquote(sprintf("  Return on Wager : %.2f", returnOnWager)))
+	    print(noquote(sprintf("Return on Capital : %.2f", returnOnCapital)))
+    }
 
     return(betCardResult)
 
